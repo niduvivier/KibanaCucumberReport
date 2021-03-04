c0ca9f3d9ed4670fb5277d9945bca6b1876d58b5
Nicolas Duvivier <nicolasg.duvivier@gmail.com>
2021-03-04 19:37:28 +0100

    First working version with TC and Steps

--- src/test/java/be/automatiqa/plugin/CustomCucumberLogger.java
+++ src/test/java/be/automatiqa/plugin/CustomCucumberLogger.java
@@ -1,13 +1,18 @@
 package be.automatiqa.plugin;
 
+import be.automatiqa.infra.RestClient;
 import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.*;
-
 import java.sql.Timestamp;
 import java.util.*;
 
 public class CustomCucumberLogger implements ConcurrentEventListener {
+    private RestClient restClient;
+
     private CucumberReport cucumberReport;
+
+    private CucumberReport.CucumberReportBuilder cucumberReportBuilder;
+    private StepReport.StepReportBuilder stepReportBuilder;
 
     private Timestamp testCaseStartTimestamp;
     private Map<UUID, Timestamp> stepStartTimestamps = new HashMap<>();
@@ -15,6 +20,7 @@ public class CustomCucumberLogger implements ConcurrentEventListener {
 
     @Override
     public void setEventPublisher(EventPublisher eventPublisher) {
+        restClient = new RestClient();
         eventPublisher.registerHandlerFor(TestCaseStarted.class, this::setUpTestCaseInfos);
         eventPublisher.registerHandlerFor(TestStepStarted.class, this::addStepStartTimestamp);
         eventPublisher.registerHandlerFor(TestStepFinished.class, this::addStepEndTimestamp);
@@ -22,23 +28,33 @@ public class CustomCucumberLogger implements ConcurrentEventListener {
     }
 
     private void setUpTestCaseInfos(TestCaseStarted event){
-        cucumberReport = new CucumberReport();
-        cucumberReport.setTestCaseName(event.getTestCase().getName());
-        cucumberReport.setTestCaseTags(event.getTestCase().getTags());
+        cucumberReportBuilder = CucumberReport.builder()
+                .testCaseName(event.getTestCase().getName())
+                .testCaseTags(event.getTestCase().getTags());
         testCaseStartTimestamp = Timestamp.from(event.getInstant());
     }
 
     private void finalizeTestCaseInfo(TestCaseFinished event){
-        System.out.println(this);
         Timestamp testCaseEndTimestamp = Timestamp.from(event.getInstant());
-        cucumberReport.setTestCaseDuration(testCaseEndTimestamp.getTime() - testCaseStartTimestamp.getTime());
+        cucumberReport = cucumberReportBuilder
+                .errorMessage(event.getResult().getError() != null ?
+                        event.getResult().getError().toString() : null)
+                .testCaseDuration(testCaseEndTimestamp.getTime() - testCaseStartTimestamp.getTime())
+                .build();
+        restClient.postReportToELK(cucumberReport);
     }
 
     private void addStepStartTimestamp(TestStepStarted event){
+        if(event.getTestStep() instanceof PickleStepTestStep){
+            PickleStepTestStep pickleStep = (PickleStepTestStep) event.getTestStep();
+            stepReportBuilder = StepReport.builder()
+                    .name(pickleStep.getStep().getText());
+        }
         stepStartTimestamps.put(event.getTestStep().getId(), Timestamp.from(event.getInstant()));
     }
 
     private void addStepEndTimestamp(TestStepFinished event){
+        cucumberReportBuilder.stepReport(stepReportBuilder.duration(10L).build());
         stepEndTimestamps.put(event.getTestStep().getId(), Timestamp.from(event.getInstant()));
     }
 
